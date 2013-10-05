6a2929fffcab97507708c367e6eb1c99dcbe27fc
vraisamis <quick.sorting.arg@gmail.com>
2013-10-05 20:00:03 +0900

    git init

---
+++ Client/src/vraisamis/jviewer/ApplicationFrame.java
@@ -0,0 +1,62 @@
+package vraisamis.jviewer;
+
+import java.awt.Dimension;
+
+import javax.swing.*;
+
+public class ApplicationFrame extends JFrame {
+    public ApplicationFrame() {
+        super();
+        // 一番外側の設定
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+       // 外側のフレームの作成
+        JSplitPane jsp = new JSplitPane();
+        jsp.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+        jsp.setPreferredSize(new Dimension(800, 600));
+        jsp.setResizeWeight(0.5);
+       // 左上の作成
+        files = new JTable(testFiles, fileFields);
+        JScrollPane sp1 = new JScrollPane(files);
+        sp1.setPreferredSize(new Dimension(400, 300));
+       // 左下の作成
+        actions = new JTable(testActions, actionFields);
+        JScrollPane sp2 = new JScrollPane(actions);
+       // 左側の作成
+        JSplitPane lPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, sp1, sp2);
+        lPane.setPreferredSize(new Dimension(400, 600));
+        lPane.setResizeWeight(0.7);
+        jsp.setLeftComponent(lPane);
+       // 右側の作成
+        graphic = new JPanel();
+        JScrollPane spGraphic = new JScrollPane(graphic, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jsp.setRightComponent(spGraphic);
+        this.getContentPane().add(jsp);
+        this.setSize(800, 600);
+   }
+
+    private static String[] fileFields = {
+        "dst", "FileName", "FileSize"
+    };
+    private static String[] actionFields = {
+        "Key", "Keyword",  "DstDirectory"
+    };
+    private String[][] testFiles = {
+        {"", "aaa", "12"},
+        {"", "aagfer", "325"}
+    };
+    private String[][] testActions = {
+        {"A", "aaaa", "ddd"},
+        {"V", "fadr3e", "feayu"},
+        {"C", "gfranjio", "bvahuio"}
+    };
+    
+    private JTable files;
+    private JTable actions;
+    private JPanel graphic;
+
+    public static void main(String[] args) {
+        ApplicationFrame appFrame = new ApplicationFrame();
+        appFrame.setTitle("Testあ");
+        appFrame.setVisible(true);
+    }
+}
