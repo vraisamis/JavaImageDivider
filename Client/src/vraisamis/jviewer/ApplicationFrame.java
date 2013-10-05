142bf67f29b99a4e24ad2ee28836e982e244460d
vraisamis <quick.sorting.arg@gmail.com>
2013-10-05 20:04:27 +0900

    somefix

--- Client/src/vraisamis/jviewer/ApplicationFrame.java
+++ Client/src/vraisamis/jviewer/ApplicationFrame.java
@@ -9,29 +9,33 @@ public class ApplicationFrame extends JFrame {
         super();
         // 一番外側の設定
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-       // 外側のフレームの作成
+        // 外側のフレームの作成
         JSplitPane jsp = new JSplitPane();
         jsp.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
         jsp.setPreferredSize(new Dimension(800, 600));
         jsp.setResizeWeight(0.5);
-       // 左上の作成
+        // 左上の作成
         files = new JTable(testFiles, fileFields);
         JScrollPane sp1 = new JScrollPane(files);
         sp1.setPreferredSize(new Dimension(400, 300));
-       // 左下の作成
+        // 左下の作成
         actions = new JTable(testActions, actionFields);
         JScrollPane sp2 = new JScrollPane(actions);
-       // 左側の作成
+        // 左側の作成
         JSplitPane lPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, sp1, sp2);
         lPane.setPreferredSize(new Dimension(400, 600));
         lPane.setResizeWeight(0.7);
         jsp.setLeftComponent(lPane);
-       // 右側の作成
+        // 右側の作成
         graphic = new JPanel();
+        
         JScrollPane spGraphic = new JScrollPane(graphic, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
         jsp.setRightComponent(spGraphic);
         this.getContentPane().add(jsp);
         this.setSize(800, 600);
+        // メニューの作成とセット
+        menuBar = this.createMenuBar();
+        this.setJMenuBar(menuBar);
    }
 
     private static String[] fileFields = {
@@ -53,10 +57,17 @@ public class ApplicationFrame extends JFrame {
     private JTable files;
     private JTable actions;
     private JPanel graphic;
+    private JMenuBar menuBar;
 
     public static void main(String[] args) {
         ApplicationFrame appFrame = new ApplicationFrame();
         appFrame.setTitle("Testあ");
         appFrame.setVisible(true);
+    }
+
+    private JMenuBar createMenuBar() {
+        JMenu mFile = new JMenu("File");
+        JMenuBar jmb = new JMenuBar();
+        return null;
     }
 }
